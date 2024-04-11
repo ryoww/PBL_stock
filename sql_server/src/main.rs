913f427e69430ba6dca7541b8bbc2ca181716210
ryoww <ryo.shimozawa.0121@gmail.com>
2024-04-11 16:00:56 +0900

    new httpserver

--- sql_server/src/main.rs
+++ sql_server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn echo(req_body: String) -> impl Responder {
+    println!("Received : {}", req_body);
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::post().to(echo)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
