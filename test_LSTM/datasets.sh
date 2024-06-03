ff0f53d7a69f3514a31ffc9f6c818d217adf1355
ryoww <ryo.shimozawa.0121@gmail.com>
2024-06-03 16:54:13 +0900

    add datasets.sh

---
+++ test_LSTM/datasets.sh
@@ -0,0 +1,3 @@
+curl -O https://raw.githubusercontent.com/khundman/telemanom/26831a05d47857e194a7725fd982d5dea5402dd4/labeled_anomalies.csv
+
+curl -O https://s3-us-west-2.amazonaws.com/telemanom/data.zip && unzip data.zip && rm data.zip
