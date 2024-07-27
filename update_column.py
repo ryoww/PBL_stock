752558417240f988c3ad1e8538d9f22072a698ab
ryoww <ryo.shimozawa.0121@gmail.com>
2024-07-28 02:06:18 +0900

    update

--- update_column.py
+++ update_column.py
@@ -12,9 +12,11 @@ def test_get_days(stock_code):
     url = f"{BASE_URL}/getdays/{stock_code}"
     response = requests.get(url)
     print(f"GET /getdays/{stock_code}:", response.status_code, response.json())
-    return response.json()
+    days = response.json()
+    dates = [entry['date'] for entry in days]
+    print(dates)
+    return dates
 
-# dates = [entry['date'] for entry in days]
 
 
 # JSONデータをforループで回す
@@ -22,14 +24,13 @@ for stock_symbol, entries in data.items():
     print(f"Stock Symbol: {stock_symbol}")
 
     days = test_get_days(stock_symbol)
-    datas = [entry['date'] for entry in days]
 
     for entry in entries:
         date = entry['date']
         update_value = entry['close']
-        print(f"Date: {date}, Close: {update_value}")
 
-        if date in datas:
+        if date in days:
+            print(f"Date: {date}, Close: {update_value}")
             data = {
                 "date" : date,
                 "column_name" : "value",
@@ -37,5 +38,5 @@ for stock_symbol, entries in data.items():
                 "stock_code" : stock_symbol
             }
 
-            response = requests.post(f"{BASE_URL}/update_column", json=data)
-            print(response.status_code, response.text)
+            # response = requests.post(f"{BASE_URL}/update_column", json=data)
+            # print(response.status_code, response.text)
