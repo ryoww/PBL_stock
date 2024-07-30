a78c57c6dc04ba2b812dda0d9aef970777d47e24
ryoww <ryo.shimozawa.0121@gmail.com>
2024-07-30 11:29:50 +0900

    update

--- update_column.py
+++ update_column.py
@@ -5,7 +5,6 @@ import requests
 with open('stocks_values.json', 'r') as file:
     data = json.load(file)
 
-
 BASE_URL = "http://192.168.1.222:8999/"
 
 def test_get_days(stock_code):
@@ -13,20 +12,19 @@ def test_get_days(stock_code):
     response = requests.get(url)
     days = response.json()
     dates = list(set(entry['date'] for entry in days))
-    # dates = [entry['date'] for entry in days]
-    print(len(days))
-    print(len(dates))
     return dates
 
-
-
-# JSONデータをforループで回す
-for stock_symbol, entries in data.items():
+# JSONデータをforループで回して送信データを収集する
+for stock_symbol, entries in reversed(data.items()):
     print(f"Stock Symbol: {stock_symbol}")
 
     days = test_get_days(stock_symbol)
 
+    # 送信するデータを収集するリスト
+    data_to_send = []
+
     for entry in entries:
+
         date = entry['date']
         update_value = entry['close']
 
@@ -35,11 +33,15 @@ for stock_symbol, entries in data.items():
             data = {
                 "date" : date,
                 "column_name" : "value",
-                "update_value" : update_value,
+                "update_value" : update_value.replace(',',''),
                 "stock_code" : stock_symbol
             }
 
             print(data)
+            # 送信データをリストに追加
+            data_to_send.append(data)
 
-            response = requests.post(f"{BASE_URL}/update_column", json=data)
-            print( response.status_code, response.text)
+    for data in data_to_send:
+        # 収集したデータを一度にPOSTリクエストで送信
+        response = requests.post(f"{BASE_URL}/update_column", json=data)
+        print(response.status_code, response.text)
