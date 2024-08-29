b5283d1449d709ab335cddea500970e6aeab3a3d
ryoww <ryo.shimozawa.0121@gmail.com>
2024-08-30 04:30:21 +0900

    add null_index

--- sql_backup.sh
+++ sql_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BACKUP_PATH="/mnt/main/Backup/SQL/stock_backup.sql"
+BACKUP_PATH="/mnt/smb/Backup/SQL/stock_backup.sql"
 
 DB_USER="stock"
 DB_PASSWORD="ryotaro1212"
 DB_NAME="stock"
 
-mysqldump -u $DB_USER -p $DB_PASSWORD > $BACKUP_PATH
+/usr/bin/mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_PATH
 
